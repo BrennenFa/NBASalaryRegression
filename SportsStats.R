0fcdc97c6d12d2508ef373e926307f14faef4b4e
Brennen Farrell <btfarre2@ncsu.edu>
2023-12-31 01:21:35 -0500

    Fully organized data for all nba teams and used rf regression, % var explained was above 65 consistently

--- SportsStats.R
+++ SportsStats.R
@@ -1,28 +1,40 @@
 install.packages('rvest')
 install.packages('dplyr')
 install.packages('ggplot2')
-
+install.packages("randomForest")
 
 library('rvest')
 library('dplyr')
 library('ggplot2')
+library(randomForest)
+
+#function --- get rid of dollar signs/commas in money
+moneyFunction <- function(data, column_name) {
+  data[[column_name]] <- gsub("\\$", "", data[[column_name]])
+  data[[column_name]] <- gsub("\\,", "", data[[column_name]])
+  data[[column_name]] <- as.numeric(data[[column_name]])
+  
+  return(data)
+}
 
 
 #create per game table
-perGame <- "https://www.basketball-reference.com/teams/ORL/2024.html"
+perGame <- "https://www.basketball-reference.com/leagues/NBA_2024_per_game.html"
 perGameTable <- perGame %>%
   read_html() %>%
-  html_element("table[id='per_game']") %>%
+  html_element("table[id='per_game_stats']") %>%
   html_table()
 
-str(perGameTable)
+#cleaning the data
+perGameTable <- perGameTable[!(perGameTable$Rk=="Rk"), ]
+perGameTable <- mutate(perGameTable, Age = as.numeric(Age))
 
 
 
 #Create salary table
-salary <- "https://www.basketball-reference.com/contracts/ORL.html"
+salary <- "https://www.basketball-reference.com/contracts/players.html"
 salaryPage <- read_html(salary)
-salaryElement <- html_element(salaryPage, "table[id='contracts']")
+salaryElement <- html_element(salaryPage, "table[id='player-contracts']")
 salaryTable <- html_table(salaryElement)
 
 #change the column names
@@ -30,18 +42,31 @@ newNames <- salaryTable[1,]
 salaryTable <- salaryTable[-c(1), ]
 salaryTable <- salaryTable[-c(nrow(salaryTable)), ]
 colnames(salaryTable) <- newNames
+salaryTable <- salaryTable[!(salaryTable$Rk=="Rk"), ]
+salaryTable <- salaryTable[!(salaryTable$`2023-24`=="Salary"), ]
+
 
 #narrow down/clean the table
-salaryTable <- select(salaryTable, Player, Age, Guaranteed)
-salaryTable <- salaryTable[!(is.na(salaryTable$Guaranteed) | salaryTable$Guaranteed==""),]
-salaryTable <- mutate(salaryTable, Guaranteed=gsub('\\$','',Guaranteed))
-salaryTable <- mutate(salaryTable, Guaranteed=gsub('\\,','',Guaranteed))
-salaryTable <- mutate(salaryTable, Age = as.numeric(Age))
-salaryTable <- mutate(salaryTable, Guaranteed = as.numeric(Guaranteed))
-str(salaryTable)
+salaryTable <- rename(salaryTable, "Current Year Salary"="2023-24")
+salaryTable <- select(salaryTable, Player, Tm, `Current Year Salary`, Guaranteed)
+print(names(salaryTable))
+salaryTable <- moneyFunction(salaryTable, "Guaranteed")
+salaryTable <- moneyFunction(salaryTable, "Current Year Salary")
 
 
+leagueTable <- inner_join(perGameTable, salaryTable, by = c("Player", "Tm"))
+plotMP <- ggplot(data=leagueTable, aes(x=MP, y=`Current Year Salary`)) + geom_point()+geom_smooth()
+plotPTS <- ggplot(data=leagueTable, aes(x=PTS, y=`Current Year Salary`)) + geom_point()+geom_smooth()
+plotAGE <- ggplot(data=leagueTable, aes(x=Age, y=`Current Year Salary`)) + geom_point()+geom_smooth()
 
-leagueTable <- inner_join(perGameTable, salaryTable, by = c("Player", "Age"))
-plot <- ggplot(data=magicTable, aes(x=MP, y=Guaranteed)) + geom_point()+geom_smooth()
-plot
+plotMP
+plotPTS
+plotAGE
+
+#machine learning model
+salaryPredictor <- randomForest(`Current Year Salary` ~ Age + MP + PTS + AST + TRB,
+                                data= leagueTable,
+                                ntree=201)      
+salaryPredictor
+plot(salaryPredictor)
+
